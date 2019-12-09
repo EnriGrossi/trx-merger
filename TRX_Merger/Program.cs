587db1367b110d2a93d090a18bab00a390cfad3c
Enri <egforspam@gmail.com>
2019-12-09 15:41:52 +0100

    changed appdomain & Completed/Passed

--- TRX_Merger/Program.cs
+++ TRX_Merger/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using TRX_Merger.ReportGenerator;
@@ -14,6 +18,26 @@ namespace TRX_Merger
     {
         public static int Main(string[] args)
         {
+            if (AppDomain.CurrentDomain.IsDefaultAppDomain())
+            {
+                // RazorEngine cannot clean up from the default appdomain...
+                Console.WriteLine("Switching to secound AppDomain, for RazorEngine...");
+                AppDomainSetup adSetup = new AppDomainSetup();
+                adSetup.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+                var current = AppDomain.CurrentDomain;
+                // You only need to add strongnames when your appdomain is not a full trust environment.
+                var strongNames = new StrongName[0];
+
+                var domain = AppDomain.CreateDomain(
+                    "MyMainDomain", null,
+                    current.SetupInformation, new PermissionSet(PermissionState.Unrestricted),
+                    strongNames);
+                var exitCode = domain.ExecuteAssembly(Assembly.GetExecutingAssembly().Location, null, args);
+                // RazorEngine will cleanup. 
+                AppDomain.Unload(domain);
+                return exitCode;
+            }
+
 
 
             if (args.Length == 0
